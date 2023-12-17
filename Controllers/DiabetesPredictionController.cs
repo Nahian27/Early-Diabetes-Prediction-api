e289a326acdf8ab2c9574ae0beac5edb87383136
Nahian27 <59230592+Nahian27@users.noreply.github.com>
2023-12-17 15:39:36 +0600

    Clean up

--- Controllers/DiabetesPredictionController.cs
+++ Controllers/DiabetesPredictionController.cs
@@ -6,8 +6,6 @@ namespace ML_WebApp.Controllers
     [Route("/")]
     public class DiabetesPredictionController : ControllerBase
     {
-
-        public required DiabetesRiskModel.ModelInput sampleData;
         public required DiabetesRiskModel.ModelOutput prediction;
 
         [HttpGet]
@@ -23,28 +21,6 @@ namespace ML_WebApp.Controllers
         [HttpPost]
         public IActionResult Predict([FromBody] DiabetesRiskModel.ModelInput data)
         {
-            //sampleData = new()
-            //{
-            //    Age = value.Age,
-            //    Alopecia = value.Alopecia,
-            //    Delayed_healing = value.Delayed_healing,
-            //    Gender = value.Gender,
-            //    Genital_thrush = value.Genital_thrush,
-            //    Irritability = value.Irritability,
-            //    Itching = value.Itching,
-            //    Muscle_stiffness = value.Muscle_stiffness,
-            //    Obesity = value.Obesity,
-            //    Partial_paresis = value.Partial_paresis,
-            //    Polydipsia = value.Polydipsia,
-            //    Polyphagia = value.Polyphagia,
-            //    Polyuria = value.Polyuria,
-            //    Sudden_weight_loss = value.Sudden_weight_loss,
-            //    Visual_blurring = value.Visual_blurring,
-            //    Weakness = value.Weakness
-
-            //};
-            //sampleData = value;
-
             prediction = DiabetesRiskModel.Predict(data);
 
             return Ok(new
